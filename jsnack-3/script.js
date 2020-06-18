d549005ec513e901dd621f76bd9eacaef854210d
emanuele5180 <macina5180>
2020-06-18 18:29:09 +0200

    inizio terzo esercizio1

--- jsnack-3/script.js
+++ jsnack-3/script.js
@@ -1,7 +1,17 @@
 //Al click su un bottone, il programma acquisisce tramite prompt un numero.
-document.getElementById('doit').addEventListener('click', function () {
+document.getElementById('doit').addEventListener("click", function () {
   var numero = prompt ("inserisci un numero");
   console.log(numero);
-})
+  if (numero > 0) {
+
+
+
+  }else if (numero < 0){
+
+  }else {
+
+  }
+
+});
 //Se il numero è positivo, il background di un p#colore diventa verde,
 //se negativo rosso e se zero blu.
