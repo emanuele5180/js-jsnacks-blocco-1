4cb7884b5ea8fa923008749c203a5943dbe45cfc
emanuele5180 <macina5180>
2020-06-18 21:31:15 +0200

    terzo esercizio

--- jsnack-3/script.js
+++ jsnack-3/script.js
@@ -1,14 +1,16 @@
 //Al click su un bottone, il programma acquisisce tramite prompt un numero.
 document.getElementById('doit').addEventListener("click", function () {
   var numero = prompt ("inserisci un numero");
+  var sfondo = document.getElementById("colore")
   console.log(numero);
   if (numero > 0) {
-
-
+    sfondo.className = "verde";
 
   }else if (numero < 0){
+    sfondo.className = "rosso";
 
   }else {
+    sfondo.className = "blu";
 
   }
 
