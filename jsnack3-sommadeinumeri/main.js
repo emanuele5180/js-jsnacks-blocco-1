809d91751eac01c48c8058fba589c95e267d0594
emanuele5180 <macina5180>
2020-06-26 15:54:19 +0200

    esercizio3 jsnack 25/06 fine

--- jsnack3-sommadeinumeri/main.js
+++ jsnack3-sommadeinumeri/main.js
@@ -1,11 +1,14 @@
 
 
-
+var risultato = 0;
 for (var i = 0; i < 5; i++) {
 
-  var inserisci = prompt("inserisci il numero");
+  var inserisci = parseInt(prompt("inserisci il numero"));
 
   console.log(inserisci);
 
-  
+  risultato += inserisci;
+
+
 }
+console.log(risultato);
